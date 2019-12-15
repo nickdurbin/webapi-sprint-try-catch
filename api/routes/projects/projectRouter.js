62293d86b2c95fe703ec2d0cec0641ac72186487
ndurbin <nick.durbin@gmail.com>
2019-12-15 11:05:50 -0500

    Initial imports for our project routes.

---
+++ api/routes/projects/projectRouter.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const Projects = require('../../../data/helpers/projectModel')
+
+const router = express.Router({ mergeParams: true })
+
+
+module.exports = router;
